8b4fada74b365c1ce3d2698bd1e6be980be8b64b
Rami Al RIhawi <rrihawi@gmail.com>
2017-11-03 14:47:28 +0100

    Part 6, exercise 1.
    * Add HelloUser.java class
    * Implement greetUser

---
+++ src/HelloUser.java
@@ -0,0 +1,8 @@
+public class HelloUser {
+    private String userName;
+
+    public HelloUser(String userName) { this.userName = userName; }
+
+    public String getUserName() { return this.userName; }
+    public void setUserName(String userName) {this.userName = userName; }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		HelloUser user;
+
+		Scanner scan = new Scanner(System.in);
+
 		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.print("Please enter your name: ");
+		user = new HelloUser(scan.nextLine().trim());
+		greetUser(user);
 	}
 
+	static void greetUser(HelloUser user) { System.out.println(String.format("Hello %s!", user.getUserName())); }
 }
