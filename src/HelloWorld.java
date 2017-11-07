5fd0da9e62977eda64380ac98204ae57a91c1b54
Neha Gopinath <nehagowri94@gmail.com>
2017-11-07 13:48:21 +0100

    Finally Push and Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 		String name;
 		Scanner in = new Scanner(System.in);
 		// TODO Auto-generated method stub
-		System.out.println("Hello ASE2017 - How are you? :)");
+		System.out.println("Hello ASE2017 - How are you now? :)");
 		System.out.println("Please enter your name   ");
 		name=in.nextLine();
 		HelloUser.greetUser(name);
